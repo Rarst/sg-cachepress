031b36b11cb3e26ec6f56b37ed6c3729b75212e2
Ivan Yordanov <ivan.yordanov@siteground.com>
2016-09-01 17:27:02 +0300

    Update sg-cachepress.php
    fix version

--- sg-cachepress.php
+++ sg-cachepress.php
@@ -9,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name:       SG CachePress
  * Description:       Through the settings of this plugin you can manage how your Wordpress interracts with NGINX and Memcached.
- * Version:           2.3.9
+ * Version:           2.3.10
  * Author:            SiteGround
  * Text Domain:       sg-cachepress
  * Domain Path:       /languages
