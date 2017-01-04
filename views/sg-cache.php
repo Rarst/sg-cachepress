9d1ca3cb93aef50ba3fdf372775f4caec88a2069
Plamen Petrov <plamen.petrov@siteground.com>
2017-01-04 17:54:44 +0200

    view added

--- views/sg-cache.php
+++ views/sg-cache.php
@@ -3,8 +3,21 @@
 		<h2><?php _e( 'SuperCacher for WordPress by SiteGround', 'sg-cachepress' ) ?></h2>		
 		<p><?php _e( 'The SuperCacher is a system that allows you to use the SiteGround dynamic cache and Memcached to optimize the performance of your WordPress. In order to take advantage of the system you should have the SuperCacher enabled at your web host plus the required cache options turned on below. For more information on the different caching options refer to the <a href="http://www.siteground.com/tutorials/supercacher/" target="_blank">SuperCacher Tutorial</a>!', 'sg-cachepress' ) ?></p>
 	</div>
-
+    
+    
 	<div class="box sgclr">
+		<h2><?php _e( 'PHP Version Status', 'sg-cachepress' ) ?></h2>
+                
+		<div class="greybox">
+			<p><?php _e( 'Checks the PHP version your WordPress site is running and whether you\'re on the fastest possible PHP version. In case you need to rollback the PHP version <a href="#">click here</a>   ', 'sg-cachepress' ) ?></p>
+			
+			<form method="post" action="<?php //menu_page_url( 'sg-cachepress-optimalphpversion' ); ?>">                            
+                            <?php submit_button( __( 'Optimal PHP Version', 'sg-cachepress' ), 'primary', 'sg-cachepress-blacklist', false );?>
+			</form>
+		</div>
+	</div>   
+    
+    	<div class="box sgclr">
 		<h2><?php _e( 'Dynamic Cache Settings', 'sg-cachepress' ) ?></h2>
 	
 		<div class="three sgclr">
@@ -41,7 +54,7 @@
 				<?php submit_button( __( 'Update the Exclude List', 'sg-cachepress' ), 'primary', 'sg-cachepress-blacklist', false );?>
 			</form>
 		</div>
-	</div>                                     
+	</div>                                      
 	<div class="box">
 		<h2><?php _e( 'Memcached Settings', 'sg-cachepress' ) ?></h2>
 		<div class="greybox">
