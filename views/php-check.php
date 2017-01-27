5330ecfaf53431e528f929a34a61fdafb5b4bd1d
Git User <git@localhost>
2017-01-27 02:01:46 -0600

    views update

--- views/php-check.php
+++ views/php-check.php
@@ -9,15 +9,10 @@ $prev_php_version = SG_WPEngine_PHPCompat::get_prev_php_version();
 
 ?>
 <div class="sgwrap">
-	<div class="box">
-		<h2><?php _e( 'SuperCacher for WordPress by SiteGround', 'sg-cachepress' ) ?></h2>		
-		<p><?php _e( 'The SuperCacher is a system that allows you to use the SiteGround dynamic cache and Memcached to optimize the performance of your WordPress. In order to take advantage of the system you should have the SuperCacher enabled at your web host plus the required cache options turned on below. For more information on the different caching options refer to the <a href="http://www.siteground.com/tutorials/supercacher/" target="_blank">SuperCacher Tutorial</a>!', 'sg-cachepress' ) ?></p>
-	</div>  
+	<div class="box" style="display:none;"></div>
+	<div class="box" style="display:none;"></div>
     
-        <div class="box" style="display:none;"></div>
-        
-    
-        <!-- START phpVersionChecker -->
+	<!-- START phpVersionChecker -->
 	<div class="box sgclr" id="phpVersionCheckerContainer" style="display: none;">
             <h2><?php _e( 'PHP Version Status', 'sg-cachepress' ) ?></h2>
 
