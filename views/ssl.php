03599409352be9f53748d797bb4384a76e9b8bdd
Git User <git@localhost>
2017-01-31 05:51:34 -0600

    SSL view edit

--- views/ssl.php
+++ views/ssl.php
@@ -7,8 +7,14 @@ $siteurlHTTPS = SG_CachePress_SSL::switchProtocol('http', 'https', $siteurl)
 	<div class="box" style="display: none;"></div>
 	<div class="box">
             <h2><?php _e( 'Force HTTPS', 'sg-cachepress' ) ?></h2>
-            <p><?php _e( 'Switching on will reconfigure your WordPress site to work entirely through HTTPS and will fix any potential mixed content issues. Note, that if you\'re including a source from a site that cannot be reached via HTTPS, this resource will not be loaded on your site.', 'sg-cachepress' ) ?></p>
-            <p><strong><?php _e( 'Important: You may have to login again after this feature is enabled!', 'sg-cachepress' ) ?></strong></p>
+            <p><?php _e( 'The switch below will reconfigure your website to use an SSL certificate. First, it will redirect all your traffic through HTTPS. Thus, your site will always load through a secure connection and you will not have non-secure versions of your pages and duplicate content. Second, it will also make the connection to any resource your site includes secure. This way the visitors will not see mixed content warning in the browser.', 'sg-cachepress' ) ?></p>
+            
+             <p><strong><?php _e( 'Note:',  'sg-cachepress') ?></strong> <?php _e( 'If your site includes a resource from an external location that cannot be reached via HTTPS this resource will no longer be loaded on your site.', 'sg-cachepress' ) ?></strong></p>
+            
+            
+            
+            
+
          
             <div class="greybox">
                                     
@@ -18,9 +24,16 @@ $siteurlHTTPS = SG_CachePress_SSL::switchProtocol('http', 'https', $siteurl)
                                ) echo 'toggleon'; else echo 'toggleoff'; ?>"></a>                    
 
                     <p id="sg-cachepress-ssl-text"><?php _e( 'Force HTTPS', 'sg-cachepress' ) ?></p>                    
-                    <p class="notcached" id="sg-cachepress-ssl-error"><?php  if ( SG_CachePress_SSL::is_partially_enabled()) 
-                        _e( 'Warning: It seems you’ve been using another plugin or manually configured your WordPress application to work over HTTPS. Please, disable all SSL forcing plugins and remove all .htaccess rules regarding SSL before you enable the option in order to avoid potential issues.', 'sg-cachepress' ) ?>
+                    
+                    <?php  if ( SG_CachePress_SSL::is_partially_enabled()) {?>
+                    <p id="sg-cachepress-ssl-error">
+                        <?php _e( 'Warning: It seems you’ve been using another plugin or manually configured your WordPress application to work over HTTPS. Please, disable all SSL forcing plugins and remove all .htaccess rules regarding SSL before you enable the option in order to avoid potential issues.', 'sg-cachepress' ) ?>
                     </p>
+                    <?php } ?> 
+                    
+                    <?php if (SG_CachePress_SSL::is_fully_enabled()) {?>
+                    <p id="sg-cachepress-logout"><strong><?php _e( 'Important:',  'sg-cachepress') ?></strong> <?php _e( 'You may have to login again after this feature is enabled!', 'sg-cachepress' ) ?></p>
+                    <?php } ?> 
                     
 
 
