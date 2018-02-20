8ca0d912a1f4475ef8d578f8cbc1bfa02f50ef1d
Git User <git@localhost>
2018-02-20 07:22:24 -0600

    design work

--- views/network-log.php
+++ views/network-log.php
@@ -1,5 +1,16 @@
-<div class="wrap">
-	<h1><?php esc_html_e( 'Log', 'sg-cachepress' ); ?></h1>
+<?php
 
-	<p><pre><?php echo esc_html( $log ); ?></pre></p>
+/** @var SG_CachePress_Performance_Tool $sg_cachepress_performance_tool */
+global $sg_cachepress_performance_tool;
+?>
+
+<div class="sgwrap">         
+	<div class="box sgclr">	
+	<h2><?php _e( 'Multisite Network Log', 'sg-cachepress' ) ?></h2>		
+	<p><?php _e( 'Here, you can see a log for the last ten changes made to your SG Optimizer configurateion either by you, or the users you have granted permissions to modify the configuration.', 'sg-cachepress' ) ?></p>
+	
+	<div class="greybox">
+		<h3><?php esc_html_e( 'Action Log', 'sg-cachepress' ); ?></h3>
+		<p><pre><?php echo esc_html( $log ); ?></pre></p>
+	</div>
 </div>
--- views/network-settings.php
+++ views/network-settings.php
@@ -10,34 +10,43 @@ $default_enable_cache    = get_site_option( 'sg-cachepress-default-enable-cache'
 $default_autoflush_cache = get_site_option( 'sg-cachepress-default-autoflush-cache', 0 );
 
 ?>
-<div class="wrap">
 
-	<h1><?php esc_html_e( 'SG Optimizer', 'sg-cachepress' ); ?></h1>
+<div class="sgwrap">         
+    	<div class="box sgclr">	
+    	
+    	<h2><?php _e( 'SG Optimizer Network Admin', 'sg-cachepress' ) ?></h2>		
+    	<p><?php _e( 'On this page, you can configure two options that will affect your entire network - Gzip Compression and Borowser Cache improved configuration. Those changes will affect every site  in your network. There are additional two default settings for newly created sites that you can configure - Dynamic Cache and AutoFlush options. Note, that you can always disable or enable both settings per site and those are simply defaults for newly added ones.', 'sg-cachepress' ) ?></p>
 
-	<h2><?php esc_html_e( '.htaccess Options', 'sg-cachepress' ); ?></h2>
 
-	<p>
-		<a id="sg-cachepress-gzip-toggle" class="<?php echo $gzip_enabled ? 'toggleon' : 'toggleoff'; ?>" href="#"></a>
-		<?php esc_html_e( 'gZip Compression', 'sg-cachepress' ); ?>
-	</p>
-	<br />
-	<p>
-		<a id="sg-cachepress-browser-cache-toggle" class="<?php echo $expires_enabled ? 'toggleon' : 'toggleoff'; ?>" href="#"></a>
-		<?php esc_html_e( 'Leverage Browser Cache', 'sg-cachepress' ); ?>
-	</p>
-	<br/>
 
-	<h2><?php esc_html_e( 'New Site Options', 'sg-cachepress' ); ?></h2>
-
-	<p>
-		<a id="sg-cachepress-default-enable-cache-toggle" class="<?php echo $default_enable_cache ? 'toggleon' : 'toggleoff'; ?>" href="#"></a>
-		<?php esc_html_e( 'Dynamic Cache', 'sg-cachepress' ); ?>
-	</p>
-	<br />
-	<p>
-		<a id="sg-cachepress-default-autoflush-cache-toggle" class="<?php echo $default_autoflush_cache ? 'toggleon' : 'toggleoff'; ?>" href="#"></a>
-		<?php esc_html_e( 'AutoFlush Cache', 'sg-cachepress' ); ?>
-	</p>
-
-	<input type="hidden" id="nonce-parameter-update" name="nonce-parameter-update" value="<?php echo wp_create_nonce( 'sg-cachepress-parameter-update' ); ?>" />
+<div class="greybox">
+	<h3><?php esc_html_e( 'Global settings for your Multisite Network', 'sg-cachepress' ); ?></h3>
+		<br />
+		<p>
+			<a id="sg-cachepress-gzip-toggle" class="<?php echo $gzip_enabled ? 'toggleon' : 'toggleoff'; ?>" href="#"></a>
+			<?php esc_html_e( 'gZip Compression', 'sg-cachepress' ); ?>
+		</p>
+		<br />
+		<p>
+			<a id="sg-cachepress-browser-cache-toggle" class="<?php echo $expires_enabled ? 'toggleon' : 'toggleoff'; ?>" href="#"></a>
+			<?php esc_html_e( 'Leverage Browser Cache', 'sg-cachepress' ); ?>
+		</p>
 </div>
+
+
+<div class="greybox">
+	<h3><?php esc_html_e( 'Default Settings for New Sites', 'sg-cachepress' ); ?></h3>
+		<br />
+		<p><a id="sg-cachepress-default-enable-cache-toggle" class="<?php echo $default_enable_cache ? 'toggleon' : 'toggleoff'; ?>" href="#"></a>
+			<?php esc_html_e( 'Dynamic Cache', 'sg-cachepress' ); ?>
+		</p>
+		<br />
+		<p><a id="sg-cachepress-default-autoflush-cache-toggle" class="<?php echo $default_autoflush_cache ? 'toggleon' : 'toggleoff'; ?>" href="#"></a>
+			<?php esc_html_e( 'AutoFlush Cache', 'sg-cachepress' ); ?>
+		</p>
+		
+		<input type="hidden" id="nonce-parameter-update" name="nonce-parameter-update" value="<?php echo wp_create_nonce( 'sg-cachepress-parameter-update' ); ?>" />
+	</div>
+</div>
+
+
